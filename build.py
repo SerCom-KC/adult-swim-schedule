bbb2f29e63419c2f3accd7c8ec8f5e9599fa06b7
SerCom_KC <szescxz@gmail.com>
2018-12-19 22:18:49 +0800

    Fix new year bug

--- build.py
+++ build.py
@@ -132,7 +132,7 @@ def generate():
         print('Fetching %s' % (url))
         allshows = etree.XML(s.get(url, timeout=3).content).xpath('//allshows/show')
         date_split = allshows[0].xpath('@date')[0].split('/')
-        if int(date_split[0]) < month:
+        if int(date_split[0]) < month or (int(date_split[0]) == 12 and month == 1):
             print('\033[32mSchedule generation completed successfully!\033[0m')
             manifest(schedules)
             return 0
